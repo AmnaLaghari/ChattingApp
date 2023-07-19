47e57c5ae72eabad1f638c2953fcba71e09e233a
AmnaLaghari <amna.laghari243@gmail.com>
2023-07-19 11:20:08 +0500

    create profile and friends models, views and urls

--- mychat/models.py
+++ mychat/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    pic = models.ImageField(upload_to="img", blank=True, null=True)
+    friend = models.ManyToManyField("Friends", related_name='my_friends')
+
+    def __str__(self) -> str:
+        return self.name
+    
+class Friends(models.Model):
+    profile = models.OneToOneField(Profile, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.profile.name
--- mychat/views.py
+++ mychat/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Profile, Friends
 
 # Create your views here.
+def index(request):
+    user = request.user.profile
+    friends = user.friend.all()
+    context={"user": user, "friends": friends}
+    return render(request, "mychat/index.html", context)
+
+def detail(request, pk):
+    friend = Friends.objects.get(profile_id = pk)
+    context={'friend':friend}
+    return render(request, "mychat/detail.html", context)
